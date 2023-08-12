4bcdd7e1bfd9a87bfaac81457c5a7b839d0496c5
salihsenturk97 <salihsenturk97@gmail.com>
2023-08-12 10:52:33 +0300

    First Commit

---
+++ src/main/java/com/example/scheduletaskexample/ScheduleTaskExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduletaskexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleTaskExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleTaskExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scheduletaskexample/ScheduleTaskExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduletaskexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleTaskExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
